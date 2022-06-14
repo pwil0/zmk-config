d1ba699868d0cb3ef1f68a44a7ba4ff8843ff951
Cesar Rojas Contreras <ocerojas@amazon.com>
2022-06-14 14:43:56 +0100

    skeletyl good

---
+++ build-skeletyl.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+ZMK_DIR="/home/fieoner/projects/zmk/app"
+CONFIG_DIR="/home/fieoner/projects/zmk-config/config"
+OUTPUT_DIR="/mnt/c/Users/Install/Documents/skeletyl-firmware"
+WIN_OUTPUT='C:\Users\Install\Documents\skeletyl-firmware'
+
+mkdir -p $OUTPUT_DIR
+
+cd $ZMK_DIR
+
+west build -p -b nice_nano_v2 -- -DZMK_CONFIG=$CONFIG_DIR -DSHIELD=skeletyl_left
+mv build/zephyr/zmk.uf2 $OUTPUT_DIR/skeletyl-left.uf2
+
+west build -p -b nice_nano_v2 -- -DZMK_CONFIG=$CONFIG_DIR -DSHIELD=skeletyl_right
+mv build/zephyr/zmk.uf2 $OUTPUT_DIR/skeletyl-right.uf2
+
+cd -
+cd $OUTPUT_DIR
+explorer.exe .
+cd -
